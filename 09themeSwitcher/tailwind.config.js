002f7d1df2e3e90a326441ed2d66f8d6b5e489f9
Arush Kumar <arushkumarbjn123@gmail.com>
2025-09-19 02:26:06 +0530

    Theme switcher

---
+++ 09themeSwitcher/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,tsx,ts,jsx}"
+  ],
+  darkMode: "class",
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
