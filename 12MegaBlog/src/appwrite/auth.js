ee8108a5cca5eefb504aa8f1a87e2d8c9af8033c
Arush Kumar <arushkumarbjn123@gmail.com>
2025-10-04 13:45:39 +0530

    Initialize 12MegaBlog project with Vite, React, and Appwrite integration

---
+++ 12MegaBlog/src/appwrite/auth.js
@@ -0,0 +1,7 @@
+import conf from '../conf.js';
+import { Client, Account, Id } from 'appwrite';
+
+export class AuthService{}
+const authService = new AuthService();
+    
+export default AuthService
